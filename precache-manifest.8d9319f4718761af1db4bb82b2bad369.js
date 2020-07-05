015442a3bb64ae7fa4a8f1de3e3bffaf81c7d609
mgornova <mgornova@gmail.com>
2020-07-05 22:12:09 +0300

    Updates

---
+++ precache-manifest.8d9319f4718761af1db4bb82b2bad369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aede761fba5dac15ac9265fa27dd28a1",
+    "url": "/drag-n-drop-test-task/index.html"
+  },
+  {
+    "revision": "906e2ae6c43b2d8e515c",
+    "url": "/drag-n-drop-test-task/static/css/main.97fdcebe.chunk.css"
+  },
+  {
+    "revision": "85e038f58c7b197f3777",
+    "url": "/drag-n-drop-test-task/static/js/2.22c88b24.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/drag-n-drop-test-task/static/js/2.22c88b24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "906e2ae6c43b2d8e515c",
+    "url": "/drag-n-drop-test-task/static/js/main.dde2a19a.chunk.js"
+  },
+  {
+    "revision": "5002bf6da7acd1bbc69c",
+    "url": "/drag-n-drop-test-task/static/js/runtime-main.2d26a752.js"
+  }
+]);
